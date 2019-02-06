a49908c6ad2882ea1fa8b4334d4c4357347ff836
soumya <padalasoumya123@gmail.com>
2019-02-06 23:01:44 +0530

    added files

--- C/Assignment1/Program3/Program3/Bitwise.cpp
+++ C/Assignment1/Program3/Program3/Bitwise.cpp
@@ -1,44 +1,42 @@
 #include<stdio.h>
-void rev(int val) {
+void reverse(int val)
+{
 	int k = val;
 	int l = val;
 	int res = k & 0x00FF;
 	res = res << 8;
 	int res1 = l & 0xFF00;
 	res1 = res1 >> 8;
-	printf("\nrev val : %x", res1 | res);
+	printf("rev value is :%x", res1 | res);
 	getchar();
 }
-void rotate(int val) {
-	int k = val;
-	int l = k & 0x000F;
-	l = l << 12;
-	//printf("\n%x l val", l);
+void rotate(int val)
+{
+	int l = val;
 	int h = val;
-	h = h >> 4;
+	l = l & 0x000F;
+	int i = l << 12;
+	h= h >> 4;
 	int j = h & 0x0FFF;
-	int res = l | j;
-	printf("\nrotate val: %x", res);
+	printf("%x", h |i );
 	getchar();
 }
-int main() {
-	int val = 0xCAFA;
-	int c = 0;
-	if (val & 1) {
-		c++;
-	}
-	for (int i = 0; i < 4; i++) {
-		if ((val >> i) & 1) {
+int main()
+{
+	int val = 0xCAFE;
+	int i,c = 0;
+	for (i = 1; i <= 4; i++)
+	{
+		if ((val >> i) & 1)
 			c++;
-		}
 	}
-	if (c >= 3) {
-		printf("case1: yes\n");
-		getchar();
-	}
+	if (c >= 3)
+		printf("yes it has atleast 3 bits of 1\n");
 	else
 		printf("no\n");
-	rev(val);
+	reverse(val);
 	rotate(val);
+	getchar();
 	return 0;
+
 }
