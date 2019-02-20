0d543a744ce824072b21de99e9f74ede4788305f
soumya <padalasoumya123@gmail.com>
2019-02-20 23:09:04 +0530

    add

--- C++/Assignment2/Program5/Program5/String_overload.cpp
+++ C++/Assignment2/Program5/Program5/String_overload.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
+using namespace std;
 class String
 {
 	char *str;
@@ -8,11 +9,65 @@ class String
 public:
 	String()
 	{
+		len = 0;
 		str = NULL;
 	}
 	String(char *s)
 	{
+		len = strlen(s);
+		str = new char[len + 1];
+		strcpy(str, s);
+	}
+	String(String &s)
+	{
+		len = strlen(s.str);
+		str = new char[(len) + 1];
+		strcpy(str, s.str);	
+	}
+	~String()
+	{
+		delete []str;
+	}
+	String operator+(String s)
+	{
 		String res;
 		res.len = len + s.len;
-		res.str=new char[res.len)+1]
-		strcpy()
+		res.str = new char[(res.len)+1];
+		strcpy(res.str, str);
+		strcat(res.str, s.str);
+		//return res;
+		cout << res;
+		return res;
+	}
+	void display()
+	{
+		cout << "string is:" << str<<endl;
+	}
+	friend ostream &operator<<(ostream &cout, String &s);
+	friend istream &operator >> (istream &in, String &s);
+};
+ostream &operator<<(ostream &cout, String &s)
+{
+	cout << "string is" << s.str<<endl;
+	return cout;
+}
+istream &operator >> (istream &in, String &s)
+{
+	cout << "enter string";
+	in >> s.str;
+	return in;
+}
+int main()
+{
+	char x[10], y[10];
+	cin >> x ;
+	cin >> y ;
+	String s1(x), s2(y);
+	//.display();
+	String s3;
+	s3 = s1 + s2;
+	//.display();
+	getchar();
+	getchar();
+	return 0;
+}
